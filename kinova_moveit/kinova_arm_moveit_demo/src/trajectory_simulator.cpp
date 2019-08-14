6bb62943aca63af2fee0cfb05370653bbeb651b1
MahdiehNejati <m.nejati@u.northwestern.edu>
2019-08-14 16:42:12 -0500

    multi-stage planning working

--- kinova_moveit/kinova_arm_moveit_demo/src/trajectory_simulator.cpp
+++ kinova_moveit/kinova_arm_moveit_demo/src/trajectory_simulator.cpp
@@ -13,6 +13,8 @@ Script for simulating different multi-stage tajectories given start and goal pos
 #include <std_msgs/String.h>
 #include <vector>
 
+#include <iostream>
+
 //#include <tf/Pose.h>
 
 #include <kinova_driver/kinova_ros_types.h>
@@ -23,32 +25,38 @@ class trajectory_simulator
     ros::NodeHandle nh_;
 
     tf::Pose Home_;
+    tf::Pose Start_; 
     tf::Pose Goal_;
     geometry_msgs::Pose target_pose_;
+    geometry_msgs::Pose start_pose_;
  
-    //moveit::planning_interface::MoveGroup group;
+    moveit::planning_interface::MoveGroup group;
     moveit_msgs::DisplayTrajectory display_trajectory;
-    //moveit::planning_interface::MoveGroup::Plan my_plan;
+    moveit::planning_interface::MoveGroup::Plan my_plan;
 
     std::vector<double> group_joint_values_;
     std::vector<double> home_joint_values_;
+    std::vector<double> start_joint_values_;
+
+    std::vector<geometry_msgs::PoseStamped> goal_pose_list_;
+    std::vector<geometry_msgs::PoseStamped>::iterator goal_list_it_;
+
     robot_state::RobotState *robot_state_;
     robot_state::RobotState *start_state_;
 
     int N_Joints = 7;
+    int plan_seq_ = 0; 
 
     void initialize_publishers()
     {
         display_publisher = nh_.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-        // display_publisher = nh_.advertise<std_msgs::String>("/test_topic", 1, true);
-
+        plan_publisher = nh_.advertise<moveit_msgs::RobotTrajectory>("/planned_path", 1, true);
     }
 
     public:
 
     ros::Publisher display_publisher;
-    moveit::planning_interface::MoveGroup group;
-    moveit::planning_interface::MoveGroup::Plan my_plan;
+    ros::Publisher plan_publisher; 
 
     trajectory_simulator(): group("arm")
     {
@@ -57,6 +65,16 @@ class trajectory_simulator
         home_joint_values_.reserve(N_Joints);
         robot_state_ = new robot_state::RobotState(group.getRobotModel());
         start_state_ = new robot_state::RobotState(group.getRobotModel());
+
+        //create home default pose
+        //double defaultHome[] = {4.71,2.84,0.00,0.75,4.62,4.48,4.88};
+        //double defaultHome[] = {-0.0004588318390768009, 2.8971940876882964, -9.007232158726985, 1.2974520262949687, 16.756792806240767, 7.685565130192222, 32.42986240826462};
+        double defaultHome[] = {0.00013466423754771029, 2.9006949211748356, 1.2987090548557987, -2.0797812006459946, -4.881186838651787, -12.566861058232385};
+        std::vector<double>::iterator it = home_joint_values_.begin();
+        for (int i = 0; i<7; i++, it++)
+        {
+            (*it) = defaultHome[i];
+        }
     };
 
     ~trajectory_simulator()
@@ -69,27 +87,20 @@ class trajectory_simulator
     /*Read Configuration File(s)*/
     void configure()
     {
-        ROS_INFO("Working!!");
-        double defaultHome[] = {4.71,2.84,0.00,0.75,4.62,4.48,4.88};
-        std::vector<double>::iterator it = home_joint_values_.begin();
-        for (int i = 0; i<7; i++, it++)
-        {
-            (*it) = defaultHome[i];
-        }
-        //home_joint_values_.assign(defaultHome,defaultHome+7);
+
     };
 
     
-    void setHome(tf::Vector3 _origin, tf::Quaternion _Q)
-    {
-        Home_.setOrigin(_origin);
-        Home_.setRotation(_Q);
-        ROS_INFO("Home set!!");
-        /* Sleep to give Rviz time to visualize the plan. */
-        sleep(5.0);
-    };
+    // void setHome(tf::Vector3 _origin, tf::Quaternion _Q)
+    // {
+    //     Home_.setOrigin(_origin);
+    //     Home_.setRotation(_Q);
+    //     ROS_INFO("Home set!!");
+    //     /* Sleep to give Rviz time to visualize the plan. */
+    //     sleep(5.0);
+    // };
 
-    void HomeRobot()
+    void SetStartStateAsHome()
     {
         // get currento state
 
@@ -117,6 +128,23 @@ class trajectory_simulator
         start_state_->printStatePositions();
     };
 
+    void SetStartState(tf::Vector3 _origin, tf::Quaternion _Q)
+    {
+        // Given a pose, find joint state values
+        Start_.setOrigin(_origin);
+        Start_.setRotation(_Q);
+        ROS_INFO("Start set!!");
+        tf::poseTFToMsg(Start_, start_pose_);
+        
+        // Find start state pose with the joint state values
+        start_state_->setFromIK(group.getRobotModel()->getJointModelGroup(group.getName()), start_pose_);
+
+        // Set the group start state as start_state)_
+        group.setStartState(*start_state_);
+        ROS_WARN("start_state after set: ");
+        start_state_->printStatePositions();
+    }
+
     void setGoal(tf::Vector3 _origin, tf::Quaternion _Q)
     {
         Goal_.setOrigin(_origin);
@@ -124,15 +152,30 @@ class trajectory_simulator
         ROS_INFO("Goal set!!");
     };
 
-    void setGoalAsHome()
+    void PushGoal(tf::Vector3 _origin, tf::Quaternion _Q)
     {
-        Goal_.setOrigin(Home_.getOrigin());
-        Goal_.setRotation(Home_.getRotation());
-        ROS_INFO("Goal set = Home!!");
+        // Given a pose, find joint state values
+        Goal_.setOrigin(_origin);
+        Goal_.setRotation(_Q);
+        ROS_INFO("new goal set!!");
+        tf::poseTFToMsg(Goal_, target_pose_);
+
+        geometry_msgs::PoseStamped tmp_pose; 
+        tmp_pose.pose = target_pose_; 
+
+        goal_pose_list_.push_back(tmp_pose); 
+
     };
 
+    // void setGoalAsHome()
+    // {
+    //     Goal_.setOrigin(Home_.getOrigin());
+    //     Goal_.setRotation(Home_.getRotation());
+    //     ROS_INFO("Goal set = Home!!");
+    // };
+
     /*Free trajectory planning*/
-    void PlanFreeMotion()
+    bool PlanFreeMotion()
     {
         ROS_INFO("Planning Motion!");
         // Planning to a Pose goal
@@ -158,9 +201,46 @@ class trajectory_simulator
         ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
         /* Sleep to give Rviz time to visualize the plan. */
         sleep(5.0);
+        return success;
 
         // TODO
         //if success update start state to funal current state.
+    };
+
+    bool PlanMultiStageFreeMotion()
+    {
+        ROS_INFO("Planning Multi Stage Motion!");
+        // Planning to a Pose goal
+        // ^^^^^^^^^^^^^^^^^^^^^^^
+        // We can plan a motion for this group to a desired pose for the 
+        // end-effector.
+        group.setPoseTargets(goal_pose_list_);
+
+        std::vector<geometry_msgs::PoseStamped> current_target_list = group.getPoseTargets();
+        for (goal_list_it_=current_target_list.begin(); goal_list_it_ < current_target_list.end(); ++goal_list_it_)
+        {
+            ROS_INFO("target pose set to: x=%1.3f \t y=%1.3f\t z=%1.3f\t xr=%1.3f\t yr=%1.3f\t zr=%1.3f\t w=%1.3f\t ", 
+            goal_list_it_->pose.position.x, goal_list_it_->pose.position.y, goal_list_it_->pose.position.z, 
+            goal_list_it_->pose.orientation.x, goal_list_it_->pose.orientation.y, goal_list_it_->pose.orientation.z, goal_list_it_->pose.orientation.w);
+        }        
+
+        // Now, we call the planner to compute the plan
+        // and visualize it.
+        // Note that we are just planning, not asking move_group 
+        // to actually move the robot.
+
+        //bool success = errorcode.val == 1;
+        bool success = (group.plan(my_plan) == moveit_msgs::MoveItErrorCodes::SUCCESS);
+        plan_seq_ = ++plan_seq_; 
+        my_plan.trajectory_.joint_trajectory.header.seq = plan_seq_; 
+        if (success)
+            plan_publisher.publish(my_plan.trajectory_);
+
+        ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
+        /* Sleep to give Rviz time to visualize the plan. */
+        sleep(5.0);
+
+        return success;
     };
 
     void VisualizePlan()
@@ -190,6 +270,13 @@ class trajectory_simulator
         group.move();
         sleep(5.0);
     };
+
+    void ClearGoals()
+    {
+        goal_pose_list_.clear(); 
+    }; 
+
+
     /**/
     /**/
 };
@@ -215,23 +302,44 @@ int main(int argc, char **argv)
 
     tf::Vector3 hposit(1.63771402836,1.11316478252, 0.134094119072);
     tf::Quaternion horient (kinova::EulerXYZ2Quaternion(0.212322831154, -0.257197618484, 0.509646713734));
-    tgsim.setHome(hposit,horient);
-
-    tgsim.HomeRobot(); // set start state
     
+    //tgsim.setHome(hposit,horient);
+    //tgsim.HomeRobot(); // set start state
+    
+    // Set the start state
+    //tgsim.setStartState(hposit,horient);
+    tgsim.SetStartStateAsHome();
 
+    
 
     tf::Vector3 gposit(0.54882, -0.30854,  0.65841);
     tf::Quaternion gorient(0.68463, -0.22436, 0.68808, 0.086576);
-    tgsim.setGoal(gposit,gorient);
-    tgsim.PlanFreeMotion(); 
+    tgsim.PushGoal(gposit, gorient); 
+    // tgsim.setGoal(gposit,gorient);
+    // tgsim.PlanFreeMotion(); 
+
+    tf::Vector3 gposit2(0.54882, -0.30854,  0.65841);
+    tf::Quaternion gorient2(0.68463, -0.22436, 0.68808, 0.1);
+    tgsim.PushGoal(gposit2, gorient2); 
+
+    tf::Vector3 gposit3(0.54882, -0.30854,  0.7);
+    tf::Quaternion gorient3(0.68463, -0.22436, 0.68808, 0.1);
+    tgsim.PushGoal(gposit3, gorient3); 
+
+
+    ROS_INFO("Starting multi-stage: ");
+    
+    tgsim.PlanMultiStageFreeMotion(); 
     //tgsim.MoveRobot2Goal();
 
-    tgsim.setGoalAsHome();
-    tgsim.PlanFreeMotion(); 
+    //tgsim.setGoalAsHome();
+    //tgsim.PlanFreeMotion(); 
     //tgsim.MoveRobot2Goal();
 
-    //tgsim.VisualizePlan();
+    sleep(5.0);
+    tgsim.VisualizePlan();
+
+    tgsim.ClearGoals(); 
 
 
     // std_msgs::String a_string;
@@ -244,7 +352,9 @@ int main(int argc, char **argv)
     //     sleep(10.0);
     // }
 
-    
+    int a;
+    std::cin >> a;
+
     ROS_INFO("Bye!!");
     ros::shutdown();
 
